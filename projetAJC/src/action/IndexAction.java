14dc279f5f12bd1d9aecf2b45fcc2f981f8bb487
qmidy <midy.quentin@live.fr>
2014-11-19 10:55:31 +0100

    Structure and libraries
    Mettre en place la structure et les librairies du projet.

---
+++ projetAJC/src/action/IndexAction.java
@@ -0,0 +1,17 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class IndexAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String hello()
+	{
+		return SUCCESS;
+	}
+	
+}
